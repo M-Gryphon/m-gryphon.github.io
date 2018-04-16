eeff7875f9af7288c1c4fc4e14bc46740159696d
Gregory Griffin <mr.gregory.griffin@gmail.com>
2018-04-16 06:05:38 +0200

    Add JavaScript, google font's link

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,9 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+    $(".trigger").click(function() {
+        $(".overlay").toggle();
+    });
+});
