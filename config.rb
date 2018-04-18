8878a1793d5e67d3e66fe7d014fa763c13d5b7b7
Gregory Griffin <mr.gregory.griffin@gmail.com>
2018-04-18 05:47:04 +0200

    Update middleman gem to deploy to gh-pages

--- config.rb
+++ config.rb
@@ -81,6 +81,7 @@ end
 # Deployment
 activate :deploy do |deploy|
   deploy.method = :git
+  deploy.branch = 'master'
   deploy.build_before = true
 
   # Optional Settings
@@ -89,3 +90,4 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
